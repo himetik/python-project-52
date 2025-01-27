7b11db6ff1e8e7600f7865d00a12df3df2da69b8
himetik <himetik@tutamail.com>
2025-01-27 19:41:56 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python -m venv .venv
+source .venv/bin/activate
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+export PATH="$HOME/.local/bin:$PATH"
+
+which uv
+
+uv pip install .
