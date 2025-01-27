3cd6a30dad5f7bdeb855bb05ff64533e0ac827ed
himetik <himetik@tutamail.com>
2025-01-27 21:58:01 +0300

    debug = false

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'python-project-52-ywus.onrender.com']
 
