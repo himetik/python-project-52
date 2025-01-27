d0ce0bd4fee33c001d56e1da2e9fff7eecf73748
himetik <himetik@tutamail.com>
2025-01-27 20:45:54 +0300

    add allowed host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'domain.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://python-project-52-ywus.onrender.com']
 
 # Application definition
 
