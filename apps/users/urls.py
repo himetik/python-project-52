4283cb29be4038011911b31d5fedc8409b4aae65
himetik <himetik@tutamail.com>
2025-01-28 18:51:27 +0300

    add delete user url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.users.views import UserIndexView, UserCreateView
+from apps.users.views import UserIndexView, UserCreateView, UserDeleteView
 
 
 urlpatterns = [
     path('', UserIndexView.as_view(), name='users'),
     path('create/', UserCreateView.as_view(), name='users_create'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='users_delete'),
 ]
