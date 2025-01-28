21125bfe9eb86591426f01877306b470d2b74e66
himetik <himetik@tutamail.com>
2025-01-28 22:19:10 +0300

    Refactor user forms

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,10 +8,9 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('first_name', 'last_name', 'username')
 
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = get_user_model()
-        fields = ('first_name', 'last_name', 'username')
+class CustomUserChangeForm(CustomUserCreationForm):
+    class Meta(CustomUserCreationForm.Meta):
+        fields = CustomUserCreationForm.Meta.fields
 
     def clean_username(self):
         return self.cleaned_data.get('username')
