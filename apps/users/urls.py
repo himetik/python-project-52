abdbc7592a29f580a539b267474a3be16d067c3f
himetik <himetik@tutamail.com>
2025-01-28 17:35:04 +0300

    add url for create user

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.users.views import UserIndexView
+from apps.users.views import UserIndexView, UserCreateView
 
 
 urlpatterns = [
     path('', UserIndexView.as_view(), name='users'),
+    path('create/', UserCreateView.as_view(), name='users_create'),
 ]
