994a81165d2a36a7bcbeb06c716c835c680a20b3
himetik <himetik@tutamail.com>
2025-01-28 19:12:20 +0300

    add user update url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.users.views import UserIndexView, UserCreateView, UserDeleteView
+from apps.users.views import UserIndexView, UserCreateView, UserDeleteView, UserUpdateView
 
 
 urlpatterns = [
     path('', UserIndexView.as_view(), name='users'),
     path('create/', UserCreateView.as_view(), name='users_create'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='users_delete'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='users_update'),
 ]
