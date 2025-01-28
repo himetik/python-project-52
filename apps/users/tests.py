0023830d9125896b8a50651c61f0bab7758181f9
himetik <himetik@tutamail.com>
2025-01-28 19:54:24 +0300

    add user delete view test

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -89,3 +89,26 @@ class UserCreateViewTest(TestCase):
             message.message == 'The user has been successfully registered'
             for message in messages
         ))
+
+
+class UserDeleteViewTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='user1', password='password123')
+        self.user2 = User.objects.create_user(username='user2', password='password123')
+        self.url = reverse('users_delete', args=[self.user1.id])
+
+    def test_user_can_delete_self(self):
+        self.client.login(username='user1', password='password123')
+        response = self.client.post(self.url, follow=True)
+        self.assertFalse(User.objects.filter(id=self.user1.id).exists())
+        self.assertRedirects(response, reverse('users'))
+        messages = list(response.context['messages'])
+        self.assertTrue(any(message.message == 'The user has been successfully deleted' for message in messages))
+
+    def test_user_cannot_delete_another_user(self):
+        self.client.login(username='user2', password='password123')
+        response = self.client.post(self.url, follow=True)
+        self.assertTrue(User.objects.filter(id=self.user1.id).exists())
+        self.assertRedirects(response, reverse('users'))
+        messages = list(response.context['messages'])
+        self.assertTrue(any(message.message == 'You are not authorized to modify another user.' for message in messages))
