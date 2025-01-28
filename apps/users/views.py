5618120366763bc34272a90f314754273c85fe45
himetik <himetik@tutamail.com>
2025-01-28 17:33:44 +0300

    add user create view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,16 @@
 from django.contrib.auth import get_user_model
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from apps.users.forms import CustomUserCreationForm
+from django.conf import settings
+from django.utils.translation import gettext as _
+
+
+class UserCreateView(SuccessMessageMixin, CreateView):
+    template_name = 'apps/users/create.html'
+    form_class = CustomUserCreationForm
+    success_url = settings.LOGIN_URL
+    success_message = _('The user has been successfully registered')
 
 
 class UserIndexView(ListView):
