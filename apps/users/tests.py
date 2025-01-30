98d6c687aae88a0fe76d3a353feae394f70e1c5c
himetik <himetik@tutamail.com>
2025-01-30 16:40:17 +0300

    refactor: move mixin in separate module

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -5,25 +5,10 @@ from django.contrib.messages import get_messages
 from django.conf import settings
 from apps.users.forms import CustomUserCreationForm
 from django.utils.translation import gettext as _
-
+from apps.users.mixins import SetUpLoggedUserMixin
 
 
 User = get_user_model()
-
-
-class SetUpLoggedUserMixin:
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            password='password123'
-        )
-        login_successful = self.client.login(
-            username='testuser',
-            password='password123'
-        )
-        if not login_successful:
-            raise Exception("Failed to log in test user")
-        super().setUp() if hasattr(super(), 'setUp') else None
 
 
 class UserIndexViewTest(TestCase):
