3e715cb14abfc092e20bf3475e81bfb4bccc84f9
himetik <himetik@tutamail.com>
2025-01-30 15:00:18 +0300

    add status model

--- apps/statuses/models.py
+++ apps/statuses/models.py
@@ -1,3 +1,10 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(('name'), max_length=200, unique=True)
+    created_at = models.DateTimeField(('created at'), default=timezone.now)
+
+    def __str__(self):
+        return self.name
