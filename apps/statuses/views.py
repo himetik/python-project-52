7b4570d8584e5042499f9cd2648b0bdc90ad90be
himetik <himetik@tutamail.com>
2025-01-30 15:39:22 +0300

    add status create view

--- apps/statuses/views.py
+++ apps/statuses/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext as _
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from apps.statuses.models import Status
+from django.contrib.messages.views import SuccessMessageMixin
+from apps.statuses.forms import StatusForm
+from django.urls import reverse_lazy
 
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
@@ -17,3 +20,10 @@ class StatusIndexView(CustomLoginRequiredMixin, ListView):
     template_name = 'apps/statuses/statuses.html'
     model = Status
     context_object_name = 'statuses'
+
+
+class StatusCreateView(CustomLoginRequiredMixin, SuccessMessageMixin, CreateView):
+    template_name = 'apps/statuses/create.html'
+    form_class = StatusForm
+    success_url = reverse_lazy('statuses')
+    success_message = _('The status has been successfully created')
