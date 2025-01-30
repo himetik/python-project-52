4a23d157c2734ac67d427ee0aaf096daca4cb673
himetik <himetik@tutamail.com>
2025-01-30 16:18:04 +0300

    add statuses update path

--- apps/statuses/urls.py
+++ apps/statuses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.statuses.views import StatusIndexView, StatusCreateView, StatusDeleteView
+from apps.statuses.views import StatusIndexView, StatusCreateView, StatusDeleteView, StatusUpdateView
 
 
 urlpatterns = [
     path('', StatusIndexView.as_view(), name='statuses'),
     path('create/', StatusCreateView.as_view(), name='statuses_create'),
     path('<int:pk>/delete/', StatusDeleteView.as_view(), name='statuses_delete'),
+    path('<int:pk>/update/', StatusUpdateView.as_view(), name='statuses_update'),
 ]
