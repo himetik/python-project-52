351e9d1f6fa97decf8e838d7be49435bb157dadc
himetik <himetik@tutamail.com>
2025-01-30 15:42:06 +0300

    add create path

--- apps/statuses/urls.py
+++ apps/statuses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.statuses.views import (StatusIndexView)
+from apps.statuses.views import StatusIndexView, StatusCreateView
 
 
 urlpatterns = [
     path('', StatusIndexView.as_view(), name='statuses'),
+    path('create/', StatusCreateView.as_view(), name='statuses_create'),
 ]
