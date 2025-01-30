286a349db11f6b5990bef372f45ebf9e6fa76a3f
himetik <himetik@tutamail.com>
2025-01-30 15:22:38 +0300

    add internationalization support via gettext_lazy

--- apps/statuses/models.py
+++ apps/statuses/models.py
@@ -1,10 +1,11 @@
 from django.utils import timezone
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(('name'), max_length=200, unique=True)
-    created_at = models.DateTimeField(('created at'), default=timezone.now)
+    name = models.CharField(_('name'), max_length=200, unique=True)
+    created_at = models.DateTimeField(_('created at'), default=timezone.now)
 
     def __str__(self):
         return self.name
