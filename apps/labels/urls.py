080b3e777c539842f0c8b6f3027752bff100ce7e
himetik <himetik@tutamail.com>
2025-02-01 17:08:20 +0300

    add labels delete url path

--- apps/labels/urls.py
+++ apps/labels/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.labels.views import LabelIndexView, LabelCreateView
+from apps.labels.views import LabelIndexView, LabelCreateView, LabelDeleteView
 
 
 urlpatterns = [
     path('', LabelIndexView.as_view(), name='labels'),
     path('create/', LabelCreateView.as_view(), name='labels_create'),
+    path('<int:pk>/delete/', LabelDeleteView.as_view(), name='labels_delete'),
 ]
