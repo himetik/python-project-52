4f639d2884de8460f2085b4e436a1c4a95aa64fb
himetik <himetik@tutamail.com>
2025-02-01 14:27:12 +0300

    add task url path

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.tasks.views import TaskIndexView, TaskCreateView, TaskDeleteView, TaskUpdateView
+from apps.tasks.views import TaskIndexView, TaskCreateView, TaskDeleteView, TaskUpdateView, TaskDetailView
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', TaskCreateView.as_view(), name='tasks_create'),
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='tasks_delete'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='tasks_update'),
+    path('<int:pk>/task/', TaskDetailView.as_view(), name='tasks_instance'),
 ]
