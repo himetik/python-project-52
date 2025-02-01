e7b5322697955b69ab63a2c60ebdf7811c7b6f42
himetik <himetik@tutamail.com>
2025-02-01 11:35:33 +0300

    add task form

---
+++ apps/tasks/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from apps.tasks.models import Task
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = [
+            'name',
+            'description',
+            'status',
+            'executor',
+            'labels',
+        ]
