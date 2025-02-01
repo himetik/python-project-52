77f5e79a67357ef9b62c383094fcfb1249e89dc0
himetik <himetik@tutamail.com>
2025-02-01 13:25:44 +0300

    add tasks update DTL template

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.tasks.views import TaskIndexView, TaskCreateView, TaskDeleteView
+from apps.tasks.views import TaskIndexView, TaskCreateView, TaskDeleteView, TaskUpdateView
 
 
 urlpatterns = [
     path('', TaskIndexView.as_view(), name='tasks'),
     path('create/', TaskCreateView.as_view(), name='tasks_create'),
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='tasks_delete'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='tasks_update'),
 ]
