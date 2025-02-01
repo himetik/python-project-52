933c21537c5a49e254851149b1b2548b66eea6bd
himetik <himetik@tutamail.com>
2025-02-01 16:27:27 +0300

    add labels url path

--- apps/labels/urls.py
+++ apps/labels/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.labels.views import LabelIndexView
+from apps.labels.views import LabelIndexView, LabelCreateView
 
 
 urlpatterns = [
     path('', LabelIndexView.as_view(), name='labels'),
+    path('create/', LabelCreateView.as_view(), name='labels_create'),
 ]
