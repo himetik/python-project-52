cba1d4472643173f750aed35cb9fe76b9cd1ce01
himetik <himetik@tutamail.com>
2025-02-01 12:24:28 +0300

    add task create url path

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.tasks.views import TaskIndexView
+from apps.tasks.views import TaskIndexView, TaskCreateView
+
 
 urlpatterns = [
     path('', TaskIndexView.as_view(), name='tasks'),
+    path('create/', TaskCreateView.as_view(), name='tasks_create'),
 ]
