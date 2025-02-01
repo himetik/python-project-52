47ace6df31340668a3af71b821afa4ef31ee0aaf
himetik <himetik@tutamail.com>
2025-02-01 16:27:03 +0300

    add labels create view

--- apps/labels/views.py
+++ apps/labels/views.py
@@ -1,9 +1,20 @@
 from apps.labels.models import Label
 from apps.main.mixins import CustomLoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from apps.labels.forms import LabelForm
+from django.utils.translation import gettext as _
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class LabelIndexView(CustomLoginRequiredMixin, ListView):
     template_name = 'apps/labels/labels.html'
     model = Label
     context_object_name = 'labels'
+
+
+class LabelCreateView(CustomLoginRequiredMixin, SuccessMessageMixin, CreateView):
+    template_name = 'apps/labels/create.html'
+    form_class = LabelForm
+    success_url = reverse_lazy('labels')
+    success_message = _('The label has been successfully created')
