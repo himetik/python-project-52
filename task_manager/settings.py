064fb5dcd907f795c3c2855599f505275739300d
himetik <himetik@tutamail.com>
2025-02-01 19:22:33 +0300

    set up rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -60,7 +60,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN', False)
+
+ROLLBAR = {
+    'access_token': ACCESS_TOKEN,
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'task_manager.urls'
 
