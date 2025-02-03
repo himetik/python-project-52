6319efd9ccc2f86c7f9a29d68017555ca083d0aa
himetik <himetik@tutamail.com>
2025-02-03 21:13:31 +0300

    remove locale middleware

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
 
