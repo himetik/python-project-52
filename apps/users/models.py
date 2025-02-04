4bcfdd871948154c6b493f1e689cc2798d191419
himetik <himetik@tutamail.com>
2025-02-04 12:12:02 +0300

    add get full user name model

---
+++ apps/users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+
+
+def get_full_user_name(self):
+    return f'{self.first_name} {self.last_name}'
+
+
+get_user_model().add_to_class('__str__', def get_full_user_name(self):
+)
