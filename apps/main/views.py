222016b211dae38615a68ee17b11039482d02afd
himetik <himetik@tutamail.com>
2025-02-05 14:53:02 +0300

    add 404 handler

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+def get_404(request, exception):
+    return render(request, '404.html', status=404)
