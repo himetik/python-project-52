12d9d35bc3a42f68718e0b3b3ec243beedd33c79
himetik <himetik@tutamail.com>
2025-02-08 12:07:53 +0300

    SetUpLoggedUserMixin

--- apps/main/mixins.py
+++ apps/main/mixins.py
@@ -6,24 +6,20 @@ from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 
 
-class SetUpLoggedUserMixin:
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            password='password123'
-        )
-        login_successful = self.client.login(
-            username='testuser',
-            password='password123'
-        )
-        if not login_successful:
-            raise Exception("Failed to log in test user")
-        super().setUp() if hasattr(super(), 'setUp') else None
-
-
 class CustomLoginRequiredMixin(LoginRequiredMixin):
     def handle_no_permission(self):
         messages.error(
             self.request, _('You are not logged in! Please sign in.')
         )
         return redirect(reverse('login'))
+
+
+class SetUpLoggedUserMixin:
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data  = {'username': 'testuser', 'password': 'testpassword'}
+        cls.user = get_user_model().objects.create_user(**cls.user_data)
+
+    def setUp(self):
+        super().setUp()
+        self.client.login(**self.user_data)
