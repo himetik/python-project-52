a0a6e5490cd52a62eff1fd44d2d460b1786b6920
Георгий Иголкин <83596372+himetik@users.noreply.github.com>
2025-02-08 19:27:48 +0300

    Add tests (#26)

---
+++ apps/statuses/tests.py
@@ -0,0 +1,133 @@
+from django.test import TestCase
+from apps.statuses.models import Status
+from apps.main.mixins import SetUpLoggedUserWithStatusMixin
+from django.urls import reverse
+from apps.tasks.models import Task
+from django.utils.translation import gettext as _
+
+
+class StatusIndexViewTest(SetUpLoggedUserWithStatusMixin, TestCase):
+    def test_status_index_view(self):
+        response = self.client.get(reverse('statuses'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_index_view_template(self):
+        response = self.client.get(reverse('statuses'))
+        self.assertTemplateUsed(response, 'apps/statuses/statuses.html')
+
+    def test_index_page_disallows_non_get_requests(self):
+        disallowed_methods = ['post', 'put', 'delete', 'patch']
+        url = reverse('statuses')
+        for method in disallowed_methods:
+            with self.subTest(method=method):
+                response = getattr(self.client, method)(url)
+                self.assertEqual(response.status_code, 405)
+
+    def test_status_list_view_empty_context(self):
+        Status.objects.all().delete()
+        response = self.client.get(reverse('statuses'))
+        self.assertIn('statuses', response.context)
+        self.assertEqual(len(response.context['statuses']), 0)
+
+    def test_status_list_view_context(self):
+        Status.objects.create(name="Another Status")
+        response = self.client.get(reverse('statuses'))
+        self.assertIn('statuses', response.context)
+        self.assertGreaterEqual(len(response.context['statuses']), 2)
+        self.assertTrue(
+            any(
+                status.name == "The Status"
+                for status in response.context['statuses']
+            )
+        )
+        self.assertTrue(
+            any(
+                status.name == "Another Status"
+                for status in response.context['statuses']
+            )
+        )
+
+
+class StatusCreateViewTest(SetUpLoggedUserWithStatusMixin, TestCase):
+    def test_status_create_view(self):
+        response = self.client.get(reverse('statuses_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_create_view_template(self):
+        response = self.client.get(reverse('statuses_create'))
+        self.assertTemplateUsed(response, 'apps/statuses/create.html')
+
+    def test_create_status_success(self):
+        data = {'name': 'Status 1'}
+        response = self.client.post(reverse('statuses_create'), data)
+        self.assertRedirects(response, reverse('statuses'))
+        self.assertTrue(Status.objects.filter(name='Status 1').exists())
+
+
+class StatusUpdateViewTest(SetUpLoggedUserWithStatusMixin, TestCase):
+    def test_status_update_view(self):
+        response = self.client.get(
+            reverse('statuses_update', args=[self.status.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_update_view_template(self):
+        response = self.client.get(
+            reverse('statuses_update', args=[self.status.pk])
+        )
+        self.assertTemplateUsed(response, 'apps/statuses/update.html')
+
+    def test_status_update_success(self):
+        updated_name = "Updated Status Name"
+        response = self.client.post(
+            reverse('statuses_update', args=[self.status.pk]),
+            {'name': updated_name},
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.status.refresh_from_db()
+        self.assertEqual(self.status.name, updated_name)
+
+
+class StatusDeleteViewTest(SetUpLoggedUserWithStatusMixin, TestCase):
+    def test_status_delete_view(self):
+        response = self.client.get(
+            reverse('statuses_delete', args=[self.status.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_delete_view_template(self):
+        response = self.client.get(
+            reverse('statuses_delete', args=[self.status.pk])
+        )
+        self.assertTemplateUsed(response, 'apps/statuses/delete.html')
+
+    def test_status_delete_success(self):
+        response = self.client.post(
+            reverse('statuses_delete', args=[self.status.pk]), 
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(Status.objects.filter(pk=self.status.pk).exists()) 
+
+    def test_delete_status_with_tasks(self):
+        self.task = Task.objects.create(
+            name='Test Task',
+            status=self.status,
+            creator=self.user,
+            executor=self.user
+        )
+        response = self.client.post(
+            reverse('statuses_delete', kwargs={'pk': self.status.pk})
+        )
+        self.assertRedirects(
+            response, reverse('statuses')
+        )
+        self.assertTrue(
+            Status.objects.filter(pk=self.status.pk).exists()
+        )
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(
+            str(messages[0]),
+            _('Unable to delete a status because it is being used')
+        )
---
+++ apps/tasks/tests.py
@@ -0,0 +1,94 @@
+from django.test import TestCase
+from apps.main.mixins import SetUpLoggedUserWithTaskMixin
+from django.urls import reverse
+from apps.tasks.models import Task
+
+
+class TaskIndexViewTest(SetUpLoggedUserWithTaskMixin, TestCase):
+    def test_status_index_view(self):
+        response = self.client.get(reverse('tasks'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_index_view_template(self):
+        response = self.client.get(reverse('tasks'))
+        self.assertTemplateUsed(response, 'apps/tasks/tasks.html')
+
+    def test_task_list_view_empty_context(self):
+        Task.objects.all().delete()
+        response = self.client.get(reverse('tasks'))
+        self.assertIn('tasks', response.context)
+        self.assertEqual(len(response.context['tasks']), 0)
+
+    def test_task_list_view_context(self):
+        response = self.client.get(reverse('tasks'))
+        self.assertIn('tasks', response.context)
+        self.assertGreaterEqual(len(response.context['tasks']), 1)
+        self.assertTrue(
+            any(
+                task.name == "The Task"
+                for task in response.context['tasks']
+            )
+        )
+
+
+class TaskCreateViewTest(SetUpLoggedUserWithTaskMixin, TestCase):
+    def test_task_create_view(self):
+        response = self.client.get(reverse('tasks_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_create_view_template(self):
+        response = self.client.get(reverse('tasks_create'))
+        self.assertTemplateUsed(response, 'apps/tasks/create.html')
+
+    def test_create_task_success(self):
+        data = {
+            'name': 'Task 1',
+            'status': self.status.id,
+            'description': 'Description 1',
+        }
+        response = self.client.post(reverse('tasks_create'), data)
+        self.assertRedirects(response, reverse('tasks'))
+        self.assertTrue(
+            Task.objects.filter(name='Task 1').exists()
+        )
+
+
+class TaskUpdateViewTest(SetUpLoggedUserWithTaskMixin, TestCase):
+    def test_task_update_view(self):
+        response = self.client.get(
+            reverse('tasks_update', args=[self.task.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_update_view_template(self):
+        response = self.client.get(
+            reverse('tasks_update', args=[self.task.pk])
+        )
+        self.assertTemplateUsed(response, 'apps/tasks/update.html')
+
+    def test_update_task_success(self):
+        data = {
+            'name': 'Brend new Task Name',
+            'status': self.status.id,
+        }
+        response = self.client.post(
+            reverse('tasks_update', kwargs={'pk': self.task.pk}), data
+        )
+        self.assertRedirects(response, reverse('tasks'))
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.name, 'Brend new Task Name')
+
+
+class TaskDeleteViewTest(SetUpLoggedUserWithTaskMixin, TestCase):
+    def test_task_delete_view(self):
+        response = self.client.get(
+            reverse('tasks_delete', args=[self.task.pk])
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_task_success(self):
+        response = self.client.post(
+            reverse('tasks_delete', args=[self.task.pk])
+        )
+        self.assertRedirects(response, reverse('tasks'))
+        self.assertFalse(Task.objects.filter(pk=self.task.pk).exists())
