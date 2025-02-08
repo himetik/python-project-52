983894641235edf5dcbb25351c3d430cbc36173f
himetik <himetik@tutamail.com>
2025-02-08 14:31:56 +0300

    .

--- apps/main/tests.py
+++ apps/main/tests.py
@@ -3,15 +3,15 @@ from django.urls import reverse
 
 
 class IndexViewTest(TestCase):
-    def test_index_success_response_code(self):
-        response = self.response = self.client.get(reverse('index'))
+    def test_index_page_loads_successfully(self):
+        response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
-    def test_index_correct_template_usage(self):
-        response = self.response = self.client.get(reverse('index'))
+    def test_index_page_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_disallowed_methods(self):
+    def test_index_page_disallows_non_get_requests(self):
         disallowed_methods = ['post', 'put', 'delete', 'patch']
         url = reverse('index')
         for method in disallowed_methods:
@@ -20,11 +20,11 @@ class IndexViewTest(TestCase):
                 self.assertEqual(response.status_code, 405)
 
 
-class ErrorsPagesTest(TestCase):
-    def test_get_404(self):
-        response = self.response = self.client.get('/unreachable/')
+class ErrorPagesTest(TestCase):
+    def test_404_page_loads_for_nonexistent_url(self):
+        response = self.client.get('/unreachable/')
         self.assertEqual(response.status_code, 404)
-    
-    def test_get_404_template_usage(self):
-        response = self.response = self.client.get('/unreachable/')
+
+    def test_404_page_uses_correct_template(self):
+        response = self.client.get('/unreachable/')
         self.assertTemplateUsed(response, '404.html')
