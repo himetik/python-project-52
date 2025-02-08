05e42b9ac150cbfe13ffe90faaeb8872388cbebb
Георгий Иголкин <83596372+himetik@users.noreply.github.com>
2025-02-08 14:32:48 +0300

    Add tests (#22)

--- apps/main/mixins.py
+++ apps/main/mixins.py
@@ -4,21 +4,9 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
-
-
-class SetUpLoggedUserMixin:
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            password='password123'
-        )
-        login_successful = self.client.login(
-            username='testuser',
-            password='password123'
-        )
-        if not login_successful:
-            raise Exception("Failed to log in test user")
-        super().setUp() if hasattr(super(), 'setUp') else None
+from apps.labels.models import Label
+from apps.statuses.models import Status
+from apps.tasks.models import Task
 
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
@@ -27,3 +15,38 @@ class CustomLoginRequiredMixin(LoginRequiredMixin):
             self.request, _('You are not logged in! Please sign in.')
         )
         return redirect(reverse('login'))
+
+
+class SetUpLoggedUserMixin:
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {'username': 'testuser', 'password': 'testpassword'}
+        cls.user = get_user_model().objects.create_user(**cls.user_data)
+
+    def setUp(self):
+        super().setUp()
+        self.client.login(**self.user_data)
+
+
+class SetUpLoggedUserWithLabelMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.label = Label.objects.create(name='The Label')
+
+
+class SetUpLoggedUserWithStatusMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.status = Status.objects.create(name='The Status')
+
+
+class SetUpLoggedUserWithTaskMixin(SetUpLoggedUserMixin):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.status = Status.objects.create(name='The Status')
+        cls.task = Task.objects.create(
+            name='The Task', status=cls.status, creator=cls.user
+        )
--- apps/main/tests.py
+++ apps/main/tests.py
@@ -1,15 +1,30 @@
-from django.urls import reverse
 from django.test import TestCase
-
-
-class Get404ViewTest(TestCase):
-    def test_get_404_renders_correct_template(self):
-        response = self.client.get('/non-existent-page/')
-        self.assertEqual(response.status_code, 404)
-        self.assertTemplateUsed(response, '404.html')
+from django.urls import reverse
 
 
 class IndexViewTest(TestCase):
-    def test_index_view_status_code(self):
+    def test_index_page_loads_successfully(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+
+    def test_index_page_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_page_disallows_non_get_requests(self):
+        disallowed_methods = ['post', 'put', 'delete', 'patch']
+        url = reverse('index')
+        for method in disallowed_methods:
+            with self.subTest(method=method):
+                response = getattr(self.client, method)(url)
+                self.assertEqual(response.status_code, 405)
+
+
+class ErrorPagesTest(TestCase):
+    def test_404_page_loads_for_nonexistent_url(self):
+        response = self.client.get('/unreachable/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_404_page_uses_correct_template(self):
+        response = self.client.get('/unreachable/')
+        self.assertTemplateUsed(response, '404.html')
