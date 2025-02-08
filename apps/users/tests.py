ea8da82d1deb59401c35f9ba6aae9252832aada3
himetik <himetik@tutamail.com>
2025-02-08 22:27:45 +0300

    .

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -4,21 +4,21 @@ from django.urls import reverse
 
 
 class UserIndexViewTest(SetUpLoggedUserMixin, TestCase):
-    def test_(self):
+    def test_users_view_returns_200(self):
         response = self.client.get(reverse('users'))
         self.assertEqual(response.status_code, 200)
 
-    def test__(self):
+    def test_users_view_uses_correct_template(self):
         response = self.client.get(reverse('users'))
         self.assertTemplateUsed(response, 'apps/users/users.html')
 
-    def test___(self):
+    def test_users_view_contains_single_user(self):
         response = self.client.get(reverse('users'))
         self.assertIn('users', response.context)
         self.assertEqual(len(response.context['users']), 1)
         self.assertEqual(response.context['users'][0], self.user)
 
-    def test____(self):
+    def test_users_view_empty_when_no_users(self):
         self.user.delete()
         response = self.client.get(reverse('users'))
         self.assertIn('users', response.context)
