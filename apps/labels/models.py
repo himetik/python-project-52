20a1ed97f67050961c7e4be947e8183a07531a64
Георгий Иголкин <83596372+himetik@users.noreply.github.com>
2025-02-08 15:46:34 +0300

    Add tests (#24)

--- apps/labels/models.py
+++ apps/labels/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Label(models.Model):
-    name = models.CharField(_('name'), max_length=180, unique=True)
+    name = models.CharField(_('name'), max_length=180, unique=True, blank=False)
     created_at = models.DateTimeField(_('created at'), default=timezone.now)
 
     def __str__(self):
--- apps/labels/tests.py
+++ apps/labels/tests.py
@@ -1,6 +1,9 @@
 from apps.main.mixins import SetUpLoggedUserWithLabelMixin
 from django.test import TestCase
 from django.urls import reverse
+from apps.labels.models import Label
+from django.contrib.messages import get_messages
+from django.utils.translation import gettext as _
 
 
 class LabelIndexViewTest(SetUpLoggedUserWithLabelMixin, TestCase):
@@ -31,3 +34,54 @@ class LabelIndexViewTest(SetUpLoggedUserWithLabelMixin, TestCase):
     def test_labels_page_displays_specific_label_name(self):
         response = self.client.get(reverse('labels'))
         self.assertContains(response, "The Label")
+
+
+class LabelCreateViewTest(SetUpLoggedUserWithLabelMixin, TestCase):
+    def test_create_label_page_is_accessible(self):
+        response = self.client.get(reverse('labels_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_label_page_uses_correct_template(self):
+        response = self.client.get(reverse('labels_create'))
+        self.assertTemplateUsed(response, 'apps/labels/create.html')
+
+    def test_create_label_success(self):
+        data = {'name': 'Unique Label'}
+        response = self.client.post(reverse('labels_create'), data, follow=True)
+        self.assertTrue(Label.objects.filter(name='Unique Label').exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertTrue(
+            any(
+                str(msg) == _('The label has been successfully created')
+                for msg in messages
+            )
+        )
+        self.assertRedirects(response, reverse('labels'))
+
+    def test_create_label_with_existing_name_fails(self):
+        existing_label = Label.objects.create(name="Duplicate Label")
+        data = {"name": existing_label.name}
+        response = self.client.post(reverse("labels_create"), data)
+        self.assertEqual(
+            Label.objects.filter(name="Duplicate Label").count(), 1
+        )
+        form = response.context.get("form")
+        self.assertIsNotNone(form)
+        self.assertTrue(form.errors)
+        self.assertIn("name", form.errors)
+        self.assertIn(
+            _("Label с таким Имя уже существует."), form.errors["name"]
+        )
+
+    def test_create_label_with_empty_name_fails(self):
+        initial_label_count = Label.objects.count()
+        data = {"name": ""}
+        response = self.client.post(reverse("labels_create"), data)
+        self.assertEqual(Label.objects.count(), initial_label_count)
+        form = response.context.get("form")
+        self.assertIsNotNone(form)
+        self.assertTrue(form.errors)
+        self.assertIn("name", form.errors)
+        self.assertIn(
+            _("Обязательное поле."), form.errors["name"]
+        )
