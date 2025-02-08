2a2893ab16ce7fbb0fe5d3052dcdfb5620f404ee
himetik <himetik@tutamail.com>
2025-02-08 22:38:15 +0300

    .

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,6 +1,7 @@
 from apps.main.mixins import SetUpLoggedUserMixin
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class UserIndexViewTest(SetUpLoggedUserMixin, TestCase):
@@ -23,3 +24,30 @@ class UserIndexViewTest(SetUpLoggedUserMixin, TestCase):
         response = self.client.get(reverse('users'))
         self.assertIn('users', response.context)
         self.assertEqual(len(response.context['users']), 0)
+
+
+class UserCreateViewTest(SetUpLoggedUserMixin, TestCase):
+    def test_create_user_view_returns_200(self):
+        response = self.client.get(reverse('users_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_view_uses_correct_template(self):
+        response = self.client.get(reverse('users_create'))
+        self.assertTemplateUsed(response, 'apps/users/create.html')
+
+    def test_create_user_view_creates_user(self):
+        User = get_user_model()
+        data = {
+            'username': 'test_user',
+            'first_name': 'Firstname',
+            'last_name': 'LastName',
+            'password1': '23977sdv',
+            'password2': '23977sdv',
+        }
+        response = self.client.post(reverse('users_create'), data)
+        self.assertRedirects(response, reverse('login'))
+        self.assertTrue(User.objects.filter(username='test_user').exists())
+        user = User.objects.get(username='test_user')
+        self.assertEqual(user.first_name, 'Firstname')
+        self.assertEqual(user.last_name, 'LastName')
+        self.assertTrue(user.check_password('23977sdv'))
