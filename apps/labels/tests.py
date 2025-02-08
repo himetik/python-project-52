1ece1184102d9a88d066614d4a858ea29d184645
himetik <himetik@tutamail.com>
2025-02-08 17:09:45 +0300

    .

--- apps/labels/tests.py
+++ apps/labels/tests.py
@@ -185,15 +185,14 @@ class LabelUpdateViewTest(SetUpLoggedUserWithLabelMixin, TestCase):
         )
         self.assertRedirects(response, reverse('labels'))
 
-    # def test_update_label_with_non_existing_id_fails(self):
-    #     non_existing_id = self.label.id + 919
-    #     response = self.client.post(
-    #         reverse('labels_update', kwargs={'pk': non_existing_id}),
-    #         {'name': 'New Name'},
-    #         follow=True
-    #     )
-    #     self.assertEqual(response.status_code, 404)
-
+    def test_update_label_with_non_existing_id_fails(self):
+        non_existing_id = self.label.id + 919
+        response = self.client.post(
+            reverse('labels_update', kwargs={'pk': non_existing_id}),
+            {'name': 'New Name'},
+            follow=True
+        )
+        self.assertEqual(response.status_code, 404)
 
     # def test_update_label_with_existing_name_fails(self):
     # def test_update_label_with_empty_name_fails(self):
