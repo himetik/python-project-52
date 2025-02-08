1e6ded0142f00cee85ea1a33ca9dd0a0747962c7
himetik <himetik@tutamail.com>
2025-02-08 14:12:38 +0300

    .

--- apps/labels/tests.py
+++ apps/labels/tests.py
@@ -7,7 +7,17 @@ class LabelIndexViewTest(SetUpLoggedUserWithLabelMixin, TestCase):
     def test_view_url_accessible_by_name(self):
         response = self.client.get(reverse('labels'))
         self.assertEqual(response.status_code, 200)
-
+        
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('labels'))
         self.assertTemplateUsed(response, 'apps/labels/labels.html')
+        
+    def test_view_context(self):
+        response = self.client.get(reverse('labels'))
+        self.assertTrue('labels' in response.context)
+        labels = response.context['labels']
+        self.assertIn(self.label, labels)
+        label_from_context = labels.first()
+        self.assertEqual(label_from_context.id, self.label.id)
+        self.assertEqual(str(label_from_context), str(self.label))
+        self.assertTrue(hasattr(label_from_context, 'created_at'))
