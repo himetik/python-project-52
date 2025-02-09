f6406ee132a027aa7204089e889e676688b370cf
himetik <himetik@tutamail.com>
2025-02-09 12:43:15 +0300

    .

--- apps/labels/forms.py
+++ apps/labels/forms.py
@@ -6,10 +6,12 @@ from django.utils.translation import gettext_lazy as _
 class LabelForm(forms.ModelForm):
     class Meta:
         model = Label
-        fields = ['name']
+        fields = ["name"]
 
     def clean_name(self):
-        name = self.cleaned_data.get('name', '').strip()
+        name = self.cleaned_data.get("name", "").strip()
+
         if not name:
-            raise forms.ValidationError(_('Label name cannot be empty.'))
+            raise forms.ValidationError(_("Label name cannot be empty."))
+
         return name
