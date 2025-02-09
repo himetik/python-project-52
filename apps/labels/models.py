48ff2f10ed38b1306ea330ac8775b81daad75c08
himetik <himetik@tutamail.com>
2025-02-09 12:43:56 +0300

    .

--- apps/labels/models.py
+++ apps/labels/models.py
@@ -4,8 +4,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Label(models.Model):
-    name = models.CharField(_('name'), max_length=180, unique=True, blank=False)
-    created_at = models.DateTimeField(_('created at'), default=timezone.now)
+    name = models.CharField(
+        _("name"),
+        max_length=180,
+        unique=True,
+        blank=False,
+    )
+    created_at = models.DateTimeField(
+        _("created at"),
+        default=timezone.now,
+    )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
