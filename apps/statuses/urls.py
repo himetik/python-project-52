4ea2e814f09a882bb07d1c0cc6e6b4b326f08adc
himetik <himetik@tutamail.com>
2025-02-09 12:37:02 +0300

    .

--- apps/statuses/urls.py
+++ apps/statuses/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from apps.statuses.views import (
     StatusIndexView,
     StatusCreateView,
-    StatusDeleteView, 
-    StatusUpdateView
+    StatusDeleteView,
+    StatusUpdateView,
 )
 
 
 urlpatterns = [
-    path('', StatusIndexView.as_view(), name='statuses'),
-    path('create/', StatusCreateView.as_view(), name='statuses_create'),
+    path('',
+         StatusIndexView.as_view(), 
+        name='statuses'),
+    path(
+        'create/',
+        StatusCreateView.as_view(),
+        name='statuses_create'),
     path(
         '<int:pk>/delete/',
         StatusDeleteView.as_view(),
